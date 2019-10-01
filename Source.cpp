40d228a8fbb4431a9927807c5759dd9b3b85803d
CoolYeebOnline <55756139+CoolYeebOnline@users.noreply.github.com>
2019-10-01 17:12:47 +0100

    Week 2 If statements and Modulus work

---
+++ Source.cpp
@@ -0,0 +1,98 @@
+
+#include <iostream>
+
+//int main() {
+//	std::cout << "Please input a number between 1 and 10!";
+//		int inputNumber{ 0 };
+//		
+//		std::cin >> inputNumber;
+//		
+//		if (inputNumber < 5) {
+//			std::cout << "the number is less than 5." << std::endl;
+//
+//		}
+//		if (inputNumber == 5) {
+//			std::cout << "the number is equal to 5." << std::endl;
+//
+//		}
+//		
+//		else if (inputNumber > 5) {
+//			std::cout << "the number is greater than 5." << std::endl;
+//
+//		}
+//		
+//		if (inputNumber == 0) {
+//			std::cout << "the number entered is 0" << std::endl;
+//		}
+//		else if (inputNumber % 2 == 0) {
+//			std::cout << "this is an Even number." << std::endl;
+//		}
+//
+//		else if (inputNumber % 2 > 0) {
+//			std::cout << "this is an Odd number." << std::endl;
+//		}
+//
+//		else {
+//			std::cout << "this is an Invalid number. ";
+//		}
+//
+//
+//
+//}
+
+
+//int main() {
+//	std::cout << "Input a number from 0-80 to recieve a grade.";
+//	int inputValue{0};
+//
+//	std::cin >> inputValue;
+//	
+//	if (inputValue == 0) {
+//		std::cout << "Your grade is a U." << std::endl;
+//
+//	}
+//	else if (inputValue > 0 && inputValue < 40) {
+//		std::cout << "Your grade is an F." << std::endl;
+//	}
+//	else if (inputValue > 40 && inputValue < 50) {
+//		std::cout << "Your grade is an E" << std::endl;
+//	}
+//	else if (inputValue > 50 && inputValue < 60) {
+//		std::cout << "Your grade is a D." << std::endl;
+//
+//	}
+//	else if (inputValue > 60 && inputValue < 70) {
+//		std::cout << "Your grade is a C." << std::endl;
+//	}
+//	else if (inputValue > 70 && inputValue < 80) {
+//		std::cout << "Your grade is a B." << std::endl;
+//
+//	}
+//	else if (inputValue == 80 && inputValue < 80) {
+//		std::cout << "Your grade is an A." << std::endl;
+//
+//	}
+//}
+
+//{
+//	std::cout << "Give me a number!";
+//	int inputNumber = 0;
+//	std::cin >> inputNumber;
+//	if (inputNumber == 0) {
+//		std::cout << "the number entered is 0" << std::endl;
+//	}
+//	else if (inputNumber % 2 == 0) {
+//		std::cout << "this is an Even number." << std::endl;
+//	}
+//
+//	else if (inputNumber % 2 > 0) {
+//		std::cout << "this is an Odd number." << std::endl;
+//	}
+//
+//	else{
+//	std::cout << "this is an Invalid number. ";
+//	}
+//
+//
+//}
+
